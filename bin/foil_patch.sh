a6e0c083d24431999871f6d876c870f52edeedf2
WSW <software@mikeg.me.uk>
2024-04-22 18:42:56 +0200

    Handle serial numbers in foil patch

--- bin/foil_patch.sh
+++ bin/foil_patch.sh
@@ -3,3 +3,4 @@ set -ex
 
 # Multiply motion IDs by 10 for hydrofoil courses
 sed -i -r 's/([A-Z]+)([0-9][0-9][0-9])([A-Z]+)/\1\20\3/' events/*/gpsdata/*/*H[1-9].csv
+sed -i -r 's/_([0-9][0-9][0-9])_/_\10_/' events/*/gpsdata/*/*H[1-9].csv
