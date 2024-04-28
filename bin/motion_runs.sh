e30981c1eb4da5b0c5e06f5e456b8dc833bd04f8
WSW <software@mikeg.me.uk>
2024-04-28 17:43:47 +0200

    Tweak motion script to include windfoil

--- bin/motion_runs.sh
+++ bin/motion_runs.sh
@@ -1,6 +1,6 @@
 YEAR=$(date +%Y)
 
-for motion in $(grep Windsurf events/$YEAR/config/motions.csv | cut -d , -f5)
+for motion in $(grep Wind events/$YEAR/config/motions.csv | cut -d , -f5)
 do
   echo $motion: $(cat events/$YEAR/gpsdata/*/* | grep -c $motion)
 done
