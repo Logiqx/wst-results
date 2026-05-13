a5a8fcb6f0080ef836cc58472b872f0a5d0b8815
WSW <software@mikeg.me.uk>
2026-05-13 09:08:32 +0200

    Add header to knights CSV

--- bin/knights-2026.sh
+++ bin/knights-2026.sh
@@ -1,2 +1,5 @@
-grep Windfoil events/2026/config/entrants.csv | cut -d, -f3,4,6 | sed 's/,/ /;s/$/,Windfoil Open/' >events/2026/sailwave/windfoil.csv
-grep Windsurf.*Youth events/2026/config/entrants.csv | cut -d, -f3,4,6 | sed 's/,/ /;s/$/,Windfoil Youths/' >events/2026/sailwave/youths.csv
+echo "Helm Name,Sail No,Fleet" >events/2026/sailwave/windfoil.csv
+echo "Helm Name,Sail No,Fleet" >events/2026/sailwave/youths.csv
+
+grep Windfoil events/2026/config/entrants.csv | cut -d, -f3,4,6 | sed 's/,/ /;s/$/,Windfoil Open/' >>events/2026/sailwave/windfoil.csv
+grep Windsurf.*Youth events/2026/config/entrants.csv | cut -d, -f3,4,6 | sed 's/,/ /;s/$/,Windfoil Youths/' >>events/2026/sailwave/youths.csv
