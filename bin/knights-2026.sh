68c4993a94e6f6d3bded328c05724a0192b26fb5
WSW <software@mikeg.me.uk>
2026-05-13 09:10:13 +0200

    Fix youth CSV

--- bin/knights-2026.sh
+++ bin/knights-2026.sh
@@ -2,4 +2,4 @@ echo "Helm Name,Sail No,Fleet" >events/2026/sailwave/windfoil.csv
 echo "Helm Name,Sail No,Fleet" >events/2026/sailwave/youths.csv
 
 grep Windfoil events/2026/config/entrants.csv | cut -d, -f3,4,6 | sed 's/,/ /;s/$/,Windfoil Open/' >>events/2026/sailwave/windfoil.csv
-grep Windsurf.*Youth events/2026/config/entrants.csv | cut -d, -f3,4,6 | sed 's/,/ /;s/$/,Windfoil Youths/' >>events/2026/sailwave/youths.csv
+grep Windsurf.*Youth events/2026/config/entrants.csv | cut -d, -f3,4,6 | sed 's/,/ /;s/$/,Windsurf Youths/' >>events/2026/sailwave/youths.csv
